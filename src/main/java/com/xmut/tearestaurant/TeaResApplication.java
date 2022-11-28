3de981c63944694fe3428c6617a5a1701b042182
Bless <hello_wsy@163.com>
2022-11-28 21:14:35 +0800

    完成了新数据库的配置，完善了读写分离

--- src/main/java/com/xmut/tearestaurant/TeaResApplication.java
+++ src/main/java/com/xmut/tearestaurant/TeaResApplication.java
@@ -1,6 +1,8 @@
 package com.xmut.tearestaurant;
 
 import lombok.extern.slf4j.Slf4j;
+import org.apache.ibatis.annotations.Mapper;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
